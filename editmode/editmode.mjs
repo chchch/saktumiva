b0b2b3b5c623d52d54695c1f0c21e40a6c20f9f4
chchch <charles.li@cnrs.fr>
2025-01-18 11:35:11 +0100

    bugfixes

--- editmode/editmode.mjs
+++ editmode/editmode.mjs
@@ -46,6 +46,10 @@ const injectCSS = () => {
     color: black;
     height: fit-content;
 }
+#topbar button:hover {
+    background-color: #eeee99;
+    border-color: #eeee00;
+}
 #topbar {
     background: linear-gradient(rgb(255,255,248) 60%, rgba(255,255,255,0));
     height: auto;
@@ -58,11 +62,14 @@ const injectCSS = () => {
 #buttoncontainer {
     top: auto;
 }
-.editbutton: {
+.editbutton {
     position: relative;
     left: -1rem;
     top: 0;
     height: 0;
+}
+.editbutton:hover svg {
+    fill:  rgb(168,81,16);
 }
 `
     );
@@ -83,7 +90,9 @@ const injectHTML = async () => {
     shadow.querySelector('.closeicon').addEventListener('click', _ => blackout.style.display = 'none');
     shadow.getElementById('collatebutton').addEventListener('click',collate);
     _state.shadowRoot = shadow;
-    fillBlocksAndAddButtons();
+    const blocks = _state.curDoc.querySelectorAll('lg[*|id],p[*|id],div[*|id],div[*|id]');
+    fillBlocks(blocks);
+    addEditButtons(blocks);
 
     const topbar = document.getElementById('topbar');
     const editbutton = document.createElement('button');
@@ -128,17 +137,20 @@ const updateChecklist = e => {
 
 };
 
-const addEditButton = id => {
-    const block = document.getElementById(id);
+const addEditButtons = blocks => {for(const block of blocks) addEditButton(block);};
+
+const addEditButton = blockel => {
+    const xmlid = blockel.getAttribute('xml:id');
+    const block = document.getElementById(xmlid);
     const editbutton = document.createElement('div');
     editbutton.className = 'editbutton';
     editbutton.innerHTML = '<svg version="1.1" width="11" height="11" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <path d="M77.926,94.924H8.217C6.441,94.924,5,93.484,5,91.706V21.997c0-1.777,1.441-3.217,3.217-3.217h34.854 c1.777,0,3.217,1.441,3.217,3.217s-1.441,3.217-3.217,3.217H11.435v63.275h63.274V56.851c0-1.777,1.441-3.217,3.217-3.217 c1.777,0,3.217,1.441,3.217,3.217v34.855C81.144,93.484,79.703,94.924,77.926,94.924z"/> <path d="M94.059,16.034L84.032,6.017c-1.255-1.255-3.292-1.255-4.547,0l-9.062,9.073L35.396,50.116 c-0.29,0.29-0.525,0.633-0.686,1.008l-7.496,17.513c-0.526,1.212-0.247,2.617,0.676,3.539c0.622,0.622,1.437,0.944,2.274,0.944 c0.429,0,0.858-0.086,1.276-0.257l17.513-7.496c0.375-0.161,0.719-0.397,1.008-0.686l35.026-35.026l9.073-9.062 C95.314,19.326,95.314,17.289,94.059,16.034z M36.286,63.79l2.928-6.821l3.893,3.893L36.286,63.79z M46.925,58.621l-5.469-5.469 L73.007,21.6l5.47,5.469L46.925,58.621z M81.511,24.034l-5.469-5.469l5.716-5.716l5.469,5.459L81.511,24.034z"/> </svg>';
-    editbutton.dataset.anno = `Edit apparatus for ${id}`;
-    editbutton.addEventListener('click',editApp.bind(null,{block: id}));
+    editbutton.dataset.anno = `Edit apparatus for ${xmlid}`;
+    editbutton.addEventListener('click',editApp.bind(null,{block: xmlid}));
     block.prepend(editbutton);
 };
 
-const fillBlocksAndAddButtons = () => {
+const fillBlocks = (blocks) => {
     const blocklist = _state.shadowRoot.getElementById('blocklist');
     blocklist.classList.add('checklist');
     blocklist.addEventListener('click',updateChecklist);
@@ -153,7 +165,7 @@ const fillBlocksAndAddButtons = () => {
     div1.appendChild(input1);
     div1.appendChild(label1);
     blocklist.appendChild(div1);
-    for(const block of _state.curDoc.querySelectorAll('lg[*|id],p[*|id],div[*|id],div[*|id]')) {
+    for(const block of blocks) {
         const xmlid = block.getAttribute('xml:id');
         const div = document.createElement('div');
         const input = document.createElement('input');
@@ -173,7 +185,6 @@ const fillBlocksAndAddButtons = () => {
         div.appendChild(label);
         div.appendChild(fileinput);
         blocklist.appendChild(div);
-        addEditButton(xmlid);
     }
 };
 
@@ -202,7 +213,7 @@ const getAlignmentFile = async e => {
     checkbox.parentNode.querySelector('.foundlabel')?.remove();
 };
 
-const editApp = (e,opts) => {
+const editApp = (opts,e) => {
     const blackout = document.getElementById('editblackout');
     blackout.style.display = 'flex';
     if(!_state.doneFindAlignments) {
@@ -213,7 +224,7 @@ const editApp = (e,opts) => {
         return;
     // else clear other checkboxes 
     for(const input of _state.shadowRoot.querySelectorAll('#blocklist input[type="checkbox"][value]')) {
-        if(opts.block === input.value)
+        if(opts.block === input.value && !input.disabled)
            input.checked = true;
         else 
             input.checked = false;
@@ -271,6 +282,9 @@ const collate = async () => {
     curarticle.parentNode.replaceChild(newDoc.querySelector('article'), curarticle);
     document.getElementById('editblackout').style.display = 'none';
     
+    const blocks = _state.curDoc.querySelectorAll('lg[*|id],p[*|id],div[*|id],div[*|id]');
+    addEditButtons(blocks);
+
     // keep clicking until the apparatus appears... pretty hacky solution
     const appbutton = document.getElementById('apparatusbutton');
     appbutton.click();
