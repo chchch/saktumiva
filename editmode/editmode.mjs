9c96748a5101fb3c1894dbc4fa8390e1bfa3197b
chchch <charles.li@cnrs.fr>
2025-03-31 11:30:40 +0200

    prettier edit button

--- editmode/editmode.mjs
+++ editmode/editmode.mjs
@@ -75,13 +75,27 @@ button:hover {
     top: auto;
 }
 .editbutton {
+    height: 1.4rem;
+    width: 1.2rem;
+    background-color: rgb(249,202,121);
+    border-color: rgb(240,202,121);
+    border-radius: 0.3rem;
+    padding: 0;
+    margin: 0;
     position: relative;
     left: -1rem;
     top: 0;
     height: 0;
 }
-.editbutton:hover svg {
-    fill:  rgb(168,81,16);
+.editbutton svg {
+    height: 0.7rem;
+    width: 0.7rem;
+    position: relative;
+    top: -0.1rem;
+}
+.editbutton:hover {
+    background-color: #eeee99;
+    border-color: #eeee00;
 }
 .edited {
     border: 1px dashed red;
@@ -163,7 +177,8 @@ const addEditButton = blockel => {
     const block = document.getElementById(xmlid);
     const editbutton = document.createElement('div');
     editbutton.className = 'editbutton';
-    editbutton.innerHTML = '<svg version="1.1" width="11" height="11" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <path d="M77.926,94.924H8.217C6.441,94.924,5,93.484,5,91.706V21.997c0-1.777,1.441-3.217,3.217-3.217h34.854 c1.777,0,3.217,1.441,3.217,3.217s-1.441,3.217-3.217,3.217H11.435v63.275h63.274V56.851c0-1.777,1.441-3.217,3.217-3.217 c1.777,0,3.217,1.441,3.217,3.217v34.855C81.144,93.484,79.703,94.924,77.926,94.924z"/> <path d="M94.059,16.034L84.032,6.017c-1.255-1.255-3.292-1.255-4.547,0l-9.062,9.073L35.396,50.116 c-0.29,0.29-0.525,0.633-0.686,1.008l-7.496,17.513c-0.526,1.212-0.247,2.617,0.676,3.539c0.622,0.622,1.437,0.944,2.274,0.944 c0.429,0,0.858-0.086,1.276-0.257l17.513-7.496c0.375-0.161,0.719-0.397,1.008-0.686l35.026-35.026l9.073-9.062 C95.314,19.326,95.314,17.289,94.059,16.034z M36.286,63.79l2.928-6.821l3.893,3.893L36.286,63.79z M46.925,58.621l-5.469-5.469 L73.007,21.6l5.47,5.469L46.925,58.621z M81.511,24.034l-5.469-5.469l5.716-5.716l5.469,5.459L81.511,24.034z"/> </svg>';
+    editbutton.appendChild(
+        document.getElementById('apparatussvg').cloneNode(true));
     editbutton.dataset.anno = `Edit apparatus for ${xmlid}`;
     editbutton.addEventListener('click',editApp.bind(null,{block: xmlid}));
     block.prepend(editbutton);
